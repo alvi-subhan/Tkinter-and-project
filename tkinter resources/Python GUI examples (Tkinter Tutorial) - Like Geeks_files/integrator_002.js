5377821b66efe3e93463c55ec64db2f608bc0ac4
alvi-subhan <alvi.subhan96@gmail.com>
2018-12-14 20:47:26 +0500

    git ignore file added

--- tkinter resources/Python GUI examples (Tkinter Tutorial) - Like Geeks_files/integrator_002.js
+++
@@ -1,1 +0,0 @@
-processGoogleToken({"newToken":"AKTT1HWK3hlvI8_1ReWQtPQiUOtfBsTeM_uKl2o2YxLTh7WW5p5emZ-_hw","validLifetimeSecs":300,"freshLifetimeSecs":300,"1p_jar":"","pucrd":"CgwIABAAGAMgACgAOAF4Aw"});
